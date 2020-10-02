6716617e6717848b72edee7fceb436f007f31ba6
Arti Virkud <avirkud@unc.edu>
2020-10-02 16:51:31 -0400

    Included read of hfevents.csv

--- Datasetup.R
+++ Datasetup.R
@@ -1,14 +1,11 @@
 library(RSQLite)
-#library(sqldf)
 library(tidyverse)
 
 #loading datasets
-#setwd("~/source_data/")
-#admissions <- read_csv("admissions.csv.gz")
+setwd("~/source_data/")
 patients <- read_csv("patients.csv.gz")
-
-icddx <- read_csv("~/source_data/physionet.org/files/mimiciv/0.4/hosp/d_icd_diagnoses.csv.gz")
-dx <- read_csv("~/source_data/physionet.org/files/mimiciv/0.4/hosp/diagnoses_icd.csv.gz")
+icddx <- read_csv("d_icd_diagnoses.csv.gz")
+dx <- read_csv("diagnoses_icd.csv.gz")
 
 #Tidy Patient dataset with heart failure patients
 hf_pat <- dx %>% filter(icd_version==9 & substr(icd_code,1,3) %in%("428") & seq_num < 3|
@@ -22,14 +19,7 @@ write_csv(hf_pat, "~/derived_data/hf_dem.csv")
 write_csv(patients, "~/derived_data/all_dem.csv")
 
 #The following code hasn't successfuly run yet, due to file size
-setwd("~/source_data/physionet.org/files/mimiciv/0.4/icu")
-charts <- read_csv("chartevents.csv.gz")
-hf_dat <- dx %>% filter(icd_version==9 & substr(icd_code,1,3) %in%("428") & seq_num < 3|
-                          icd_version==10 & substr(icd_code,1,3) %in%("I50") & seq_num < 3) %>% 
-  select(subject_id, hadm_id, seq_num) %>%
-  arrange(subject_id, hadm_id, seq_num) %>% 
-  left_join(., patients) %>% left_join(., charts)
-write_csv(hf_dat, "~/derived_data/hf_data.csv")
+setwd("~/derived_data")
+#This dataset was developed in sqlite using bash code (see sqlite)
+hfevents <- read_csv("hfevents.csv")
 
-icu_chart <- dbConnect(RSQLite::SQLite(), "events.db")
-dbGetQuery(icu_chart, "SELECT * FROM chart LIMIT 10")
