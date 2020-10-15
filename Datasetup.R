a94fe77b3f1bd31f2c17d3e9249d46a408eb1f32
Arti Virkud <avirkud@unc.edu>
2020-10-14 23:47:20 -0400

    Build RX csv file

--- Datasetup.R
+++ Datasetup.R
@@ -7,25 +7,30 @@ patients <- read_csv("patients.csv")
 icddx <- read_csv("d_icd_diagnoses.csv")
 dx <- read_csv("diagnoses_icd.csv")
 admissions <- read_csv("admissions.csv")
-chartlist <- read_csv("d_items.csv.gz")
+chartlist <- read_csv("d_items.csv")
+rx <- read_csv("prescriptions.csv")
 
 
 #Tidy Patient dataset with heart failure patients, distinct by patients
-hf_patA <- dx %>% filter(icd_version==9 & substr(icd_code,1,3) %in%("428") & seq_num < 3|
+A <- dx %>% filter(icd_version==9 & substr(icd_code,1,3) %in%("428") & seq_num < 3|
                           icd_version==10 & substr(icd_code,1,3) %in%("I50") & seq_num < 3) %>% 
   select(subject_id, seq_num) %>%
   arrange(subject_id, seq_num) %>% 
   left_join(., patients) %>% 
-  distinct(., subject_id, .keep_all = TRUE) 
-hf_adm <- hf_patA %>% 
-  left_join(.,admissions)
-hf_dem <- hf_adm %>% 
-  select(-hadm_id, -admittime, -dischtime, -admission_type, -admission_location, 
-         discharge_location, edregtime, edouttime, hospital_expire_flag) %>% 
-  group_by(subject_id) %>% 
-  count(subject_id,name=visit_num)
+  distinct(., subject_id, .keep_all = TRUE)
 
-write_csv(hf_pat, "~/derived_data/hf_dem.csv")
+adm_dem <- admissions %>% select(subject_id, language, marital_status, ethnicity) %>% 
+  group_by(subject_id) %>% slice(1)
+# To find distinct number of subjects nrow(distinct(df,var1))
+
+hf_dem <- A %>% left_join(.,adm_dem) 
+hf_rx <- A %>%
+    left_join(.,rx)
+
+write_csv(hf_dem, "~/derived_data/hf_dem.csv")
+write_csv(hf_rx, "~/derived_data/hf_rx.csv")
+
+all_dem <- patients %>% left_join()
 write_csv(patients, "~/derived_data/all_dem.csv")
 
 setwd("~/derived_data")
