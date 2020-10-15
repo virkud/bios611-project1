05e093873bd24dc317b444de0df165d8d52968c7
Arti Virkud <avirkud@unc.edu>
2020-10-14 23:50:58 -0400

    Update the breaks for the pyramids

--- pop_pyramids.R
+++ pop_pyramids.R
@@ -28,7 +28,7 @@ p1
 all_pat1 <- all_pat %>% 
   group_by(anchor_age, gender) %>% 
   tally(name="pop")
-all_pat2 <- all_pat1 %>% mutate (Population = ifelse(gender=="M",hf_pat1$pop*-1,hf_pat1$pop)) %>% 
+all_pat2 <- all_pat1 %>% mutate(Population = ifelse(gender=="M",hf_pat1$pop*-1,hf_pat1$pop)) %>% 
   select(anchor_age, gender, Population) %>% 
   dplyr::rename(Age=anchor_age, Sex=gender)
 
@@ -36,8 +36,8 @@ p2 <- ggplot(all_pat2, aes(x = Age, y = Population, fill = Sex)) +
   ggtitle("Population pyramid of all ICU patients") +
   geom_bar(data=subset(all_pat2, Sex == "F"), stat = "identity") + 
   geom_bar(data=subset(all_pat2, Sex == "M"), stat = "identity") +
-  scale_y_continuous(breaks = seq(-700, 700, 100), 
-                     labels = paste0(as.character(c(seq(700, 0, -100), seq(100, 700, 100))))) + 
+  scale_y_continuous(breaks = seq(-700, 400, 100), 
+                     labels = paste0(as.character(c(seq(700, 0, -100), seq(100, 400, 100))))) + 
   coord_flip() + 
   scale_fill_brewer(palette = "Set1") + 
   theme_bw()
