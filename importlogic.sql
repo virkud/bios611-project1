b1f050e336768de124b43703bae0d88d90616578
Arti Virkud <avirkud@unc.edu>
2020-10-14 22:47:42 -0400

    Delete importlogic.sql since updated code in sqlcode

--- importlogic.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE if exists chart;
-CREATE TABLE chart (subject_id INTEGER,hadm_id INTEGER, stay_id INTEGER,
-          charttime TEXT, storetime TEXT, itemid INTEGER, value INTEGER, valuenum INTEGER,
-           valueuom TEXT, warning INTEGER);
-  
-.mode csv
-.import file.headless.csv chart
