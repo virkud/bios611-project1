eab0955d04b24d9d6c305fce460bdac9fc4c3793
Arti Virkud <avirkud@unc.edu>
2020-10-17 23:15:13 -0400

    Add shortcuts for bash to build docker and git

---
+++ aliases.sh
@@ -0,0 +1,12 @@
+# handy aliases for working with the docker file
+# and doing other stuff
+
+alias bu='docker build . -t p1-env --build-arg linux_user_pwd=yougotthis'
+alias dr='docker run -v `pwd`:/home/rstudio -p 8787:8787 -e PASSWORD=yougotthis -t p1-env'
+alias hc='docker run -p 8711:8000 -v `pwd`:/host -it p1-env hovercraft /host/slides.rst'
+alias hcb='docker run -v `pwd`:/host -it p1-env hovercraft /host/slides.rst /host/html_presentation'
+
+alias r='docker run -v `pwd`:/home/rstudio -e PASSWORD=yougotthis -it p1-env sudo -H -u rstudio /bin/bash -c "cd ~/; R"'
+alias b='docker run -v `pwd`:/home/rstudio -e PASSWORD=yougotthis -it p1-env sudo -H -u rstudio /bin/bash -c "cd ~/; /bin/bash"'
+
+alias gits='ssh-agent bash; ssh-add ~/storage/example-keys/id_rsa'
