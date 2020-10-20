add1c00c3b7ea0ba8bb72b700bb4df41c9bf617d
Arti Virkud <avirkud@unc.edu>
2020-10-19 23:42:23 -0400

    Added summary stats and modified ethnicity labels.

--- demographic_stats.R
+++ demographic_stats.R
@@ -1,18 +1,27 @@
 library(tidyverse)
 
 #Setup datasets from Datasetup.R
-all_dem <- read_csv("~/derived_data/all_dem.csv")
-hf_dem<- read_csv("~/derived_data/hf_dem.csv")
-hfevents <- read_csv("~/derived_data/hfevents.csv") %>% select(-`hadm_id:1`, -`subject_id:1`)
+all_dem <- read_csv("derived_data/all_dem.csv")
+hf_dem<- read_csv("derived_data/hf_dem.csv")
+hf_rx<- read_csv("derived_data/hf_rx.csv")
+#hfevents <- read_csv("derived_data/hfevents.csv") %>% select(-`hadm_id:1`, -`subject_id:1`)
 
 #What do HF patients look like:
-p1_eth <- hf_dem %>% mutate()
-  group_by(ethnicity) %>% 
-  summarise(mean=mean(anchor_age), min=min(anchor_age), max=max(anchor_age))
-
+summary(hf_dem)
+hf_dem %>% count(gender) %>%  mutate(prop = 100*prop.table(n))
+hf_dem %>% count(ethnicity) %>%  mutate(prop = 100*prop.table(n))
+hf_dem %>% count(marital_status) %>%  mutate(prop = 100*prop.table(n))
+hf_dem %>% count(language) %>%  mutate(prop = 100*prop.table(n))
+p1_eth <- hf_dem %>%
+  group_by(ethnicity)%>%
+  summarise(mean=mean(anchor_age), min=min(anchor_age), max=max(anchor_age)) 
+  
 p1 <- ggplot(p1_eth) +
-  geom_bar( aes(x=ethnicity, y=mean), stat="identity", fill="skyblue", alpha=0.7) +
+  geom_bar(aes(x=ethnicity, y=mean), stat="identity", fill="skyblue", alpha=0.7) +
   geom_errorbar(aes(x=ethnicity, ymin=min, ymax=max), width=0.4, colour="orange", alpha=0.9, size=1.3) +
-  title ("Average age by ethnicity")
+  xlab("Ethnicity") +
+  ylab("Average age") +
+  scale_x_discrete(labels = c('AI/AN', 'Asian','Black','HL','Other','Unable','Unknown','White'))+
+  ggtitle("Average age by ethnicity")
 ggsave("figures/eth_sex.png",plot=p1)
 p1
