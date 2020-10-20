01f8d9e045035a96d86ae95c409672b21835977a
Arti Virkud <avirkud@unc.edu>
2020-10-19 23:39:29 -0400

    Added datasetup for prescription meds

--- Datasetup.R
+++ Datasetup.R
@@ -1,14 +1,12 @@
-library(RSQLite)
 library(tidyverse)
 
 #loading datasets
-setwd("~/source_data/")
-patients <- read_csv("patients.csv")
-icddx <- read_csv("d_icd_diagnoses.csv")
-dx <- read_csv("diagnoses_icd.csv")
-admissions <- read_csv("admissions.csv")
-chartlist <- read_csv("d_items.csv")
-rx <- read_csv("prescriptions.csv")
+patients <- read_csv("source_data/patients.csv")
+icddx <- read_csv("source_data/d_icd_diagnoses.csv")
+dx <- read_csv("source_data/diagnoses_icd.csv")
+admissions <- read_csv("source_data/admissions.csv")
+chartlist <- read_csv("source_data/d_items.csv")
+rx <- read_csv("source_data/prescriptions.csv")
 
 
 #Tidy Patient dataset with heart failure patients, distinct by patients
@@ -19,7 +17,7 @@ A <- dx %>% filter(icd_version==9 & substr(icd_code,1,3) %in%("428") & seq_num <
   left_join(., patients) %>% 
   distinct(., subject_id, .keep_all = TRUE)
 
-adm_dem <- admissions %>% select(subject_id, language, marital_status, ethnicity) %>% 
+adm_dem <- admissions %>% select(subject_id, language, marital_status, ethnicity, deathtime) %>% 
   group_by(subject_id) %>% slice(1)
 # To find distinct number of subjects nrow(distinct(df,var1))
 
@@ -27,24 +25,22 @@ hf_dem <- A %>% left_join(.,adm_dem)
 hf_rx <- A %>%
     left_join(.,rx)
 
-write_csv(hf_dem, "~/derived_data/hf_dem.csv")
-write_csv(hf_rx, "~/derived_data/hf_rx.csv")
+write_csv(hf_dem, "derived_data/hf_dem.csv")
+write_csv(hf_rx, "derived_data/hf_rx.csv")
 
-all_dem <- patients %>% left_join()
-write_csv(patients, "~/derived_data/all_dem.csv")
+all_dem <- patients %>% left_join(.,adm_dem)
+write_csv(patients, "derived_data/all_dem.csv")
 
-setwd("~/derived_data")
 #This dataset was developed in sqlite using bash code (see sqlite)
-hfevents <- read_csv("hfevents.csv") %>% select(-`hadm_id:1`, -`subject_id:1`)
+#hfevents <- read_csv("derived_data/hfevents.csv") %>% select(-`hadm_id:1`, -`subject_id:1`)
 
 #List of HF events
+#hflist <- hfevents %>% select(itemid) %>% 
+#  left_join(chartlist) %>% distinct(., itemid, .keep_all = TRUE) 
 
-hflist <- hfevents %>% select(itemid) %>% 
-  left_join(chartlist) %>% distinct(., itemid, .keep_all = TRUE) 
+#ABPs1 <- hfevents %>% filter(itemid==220050) %>% group_by(subject_id, hadm_id) %>% 
+#          select(-seq_num, -icd_code, -icd_version, -storetime, -stay_id, -`hadm_id:1`)
+#top_n(ABPs1,10)
 
-ABPs1 <- hfevents %>% filter(itemid==220050) %>% group_by(subject_id, hadm_id) %>% 
-          select(-seq_num, -icd_code, -icd_version, -storetime, -stay_id, -`hadm_id:1`)
-top_n(ABPs1,10)
-#Medications among HF patients
 
 
