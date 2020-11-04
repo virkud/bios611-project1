8365097f7912730a5ebb7a844d2f72d47f690bd3
Arti Virkud <avirkud@unc.edu>
2020-11-04 11:35:59 -0500

    Revise name of the plots

--- demographic_stats.R
+++ demographic_stats.R
@@ -3,8 +3,6 @@ library(tidyverse)
 #Setup datasets from Datasetup.R
 all_dem <- read_csv("derived_data/all_dem.csv")
 hf_dem<- read_csv("derived_data/hf_dem.csv")
-hf_rx<- read_csv("derived_data/hf_rx.csv")
-#hfevents <- read_csv("derived_data/hfevents.csv") %>% select(-`hadm_id:1`, -`subject_id:1`)
 
 #What do HF patients look like:
 summary(hf_dem)
@@ -16,12 +14,12 @@ p1_eth <- hf_dem %>%
   group_by(ethnicity)%>%
   summarise(mean=mean(anchor_age), min=min(anchor_age), max=max(anchor_age)) 
   
-p1 <- ggplot(p1_eth) +
+plot_eth <- ggplot(p1_eth) +
   geom_bar(aes(x=ethnicity, y=mean), stat="identity", fill="skyblue", alpha=0.7) +
   geom_errorbar(aes(x=ethnicity, ymin=min, ymax=max), width=0.4, colour="orange", alpha=0.9, size=1.3) +
   xlab("Ethnicity") +
   ylab("Average age") +
   scale_x_discrete(labels = c('AI/AN', 'Asian','Black','HL','Other','Unable','Unknown','White'))+
   ggtitle("Average age by ethnicity")
-ggsave("figures/eth_sex.png",plot=p1)
-p1
+ggsave("figures/eth_sex.png",plot=plot_eth)
+plot_eth
