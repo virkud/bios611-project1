bd78255a6e7bc677587d0016332bcece409cc4e4
Arti Virkud <avirkud@unc.edu>
2020-11-06 14:32:58 -0500

    Copy of demographic_stats R file with simulation data reference

---
+++ sim_demographic_stats.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+#Setup datasets from Datasetup.R
+all_dem <- read_csv("derived_data/sim_all_dem.csv")
+hf_dem<- read_csv("derived_data/sim_hf_dem.csv")
+
+#What do HF patients look like:
+summary(hf_dem)
+hf_dem %>% count(gender) %>%  mutate(prop = 100*prop.table(n))
+hf_dem %>% count(ethnicity) %>%  mutate(prop = 100*prop.table(n))
+hf_dem %>% count(marital_status) %>%  mutate(prop = 100*prop.table(n))
+hf_dem %>% count(language) %>%  mutate(prop = 100*prop.table(n))
+p1_eth <- hf_dem %>%
+  group_by(ethnicity)%>%
+  summarise(mean=mean(anchor_age), min=min(anchor_age), max=max(anchor_age)) 
+  
+plot_eth <- ggplot(p1_eth) +
+  geom_bar(aes(x=ethnicity, y=mean), stat="identity", fill="skyblue", alpha=0.7) +
+  geom_errorbar(aes(x=ethnicity, ymin=min, ymax=max), width=0.4, colour="orange", alpha=0.9, size=1.3) +
+  xlab("Ethnicity") +
+  ylab("Average age") +
+  scale_x_discrete(labels = c('AI/AN', 'Asian','Black','HL','Other','Unable','Unknown','White'))+
+  ggtitle("Average age by ethnicity")
+ggsave("figures/sim_eth_sex.png",plot=plot_eth)
+plot_eth
