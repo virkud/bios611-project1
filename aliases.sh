6e53c52b9c321afb24eba9b1b31f6a107b6c60f2
Arti Virkud <avirkud@unc.edu>
2020-11-17 14:08:06 -0500

    Update name of docker instance for rshiny app

--- aliases.sh
+++ aliases.sh
@@ -17,5 +17,5 @@ alias g3='git config --global user.email "avirkud@unc.edu"'
 alias g4='git config --global user.name "Arti Virkud"'
 
 start_shiny(){
-    docker run -p $2:$2 -v `pwd`:/home/rstudio -e PASSWORD=$SECRET_PWD -it l17 sudo -H -u rstudio /bin/bash -c "cd ~/; PORT=$2 make $1"
+    docker run -p $2:$2 -v `pwd`:/home/rstudio -e PASSWORD=$SECRET_PWD -it shiny sudo -H -u rstudio /bin/bash -c "cd ~/; PORT=$2 make $1"
 }
